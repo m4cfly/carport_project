56baaecd46f0601f795328ec2034ec4c194f84f3
Davud05 <Drassuil@gmail.com>
2024-05-06 00:29:38 +0200

    Added html and css, updated main and setup Javalin and thymeleaf.
    And added packages like config and resources.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,19 @@
-package app;
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public static void main(String[] args)
+{
+    // Initializing Javalin and Jetty webserver
+
+    Javalin app = Javalin.create(config -> {
+        config.staticFiles.add("/public");
+        config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+        config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+    }).start(7070);
+
+    // Routing
+
+    app.get("/", ctx ->  ctx.render("index.html"));
 }
