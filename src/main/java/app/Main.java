191ef2fab756c59be160e66cf60c7c89dc6647f2
Davud05 <Drassuil@gmail.com>
2024-05-06 12:31:48 +0200

    updated main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,9 +4,18 @@ import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
+import org.postgresql.jdbc2.optional.ConnectionPool;
+import app.persistence.ConnectionPool;
+
 
 public class Main {
 
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
 public static void main(String[] args)
 {
