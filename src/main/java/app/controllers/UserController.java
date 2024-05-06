e875cbf03973f4eea8e4520d1fa8f0710aff4ac5
Davud05 <Drassuil@gmail.com>
2024-05-06 12:11:53 +0200

    updated controller

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,5 +1,8 @@
 package app.controllers;
 
+import io.javalin.Javalin;
+import org.postgresql.jdbc2.optional.ConnectionPool;
+
 public class UserController {
 
     public static void addRoutes (Javalin app, ConnectionPool connectionPool) {
