bf4da7ed9c4387dc3902f84edae553b5e3cd1016
Davud05 <Drassuil@gmail.com>
2024-05-06 12:12:53 +0200

    updated controller

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -6,7 +6,7 @@ import org.postgresql.jdbc2.optional.ConnectionPool;
 public class OrderController {
 
     public static void addRoutes (Javalin app, ConnectionPool connectionPool) {
-        app.post("/search", ctx -> search(ctx, connectionPool));
+        app.post("/search", ctx -> addRoutes((Javalin) ctx, connectionPool));
         app.get("/search", ctx -> ctx.render("index.html"));
     }
 
