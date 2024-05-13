d1a45492f52ae1e9af2b55771ada410ee78a5eb5
Jenjolen <cph-jn466@cphbusiness.dk>
2024-05-13 15:29:29 +0200

    Merged use-case-1 onto main branch, fixed merge conflicts and double main classes

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,32 +1,37 @@
 package app;
-
 import app.config.SessionConfig;
-
 import app.config.ThymeleafConfig;
+import app.controllers.CarportController;
+import app.controllers.UserController;
+
+
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
-// Correct the import to use your custom ConnectionPool
-import app.persistence.ConnectionPool;
+
 
 public class Main {
-
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "carport";
 
-    // Ensure that ConnectionPool is your custom singleton class
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args)
+    {
         // Initializing Javalin and Jetty webserver
+
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
-            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
         // Routing
-        app.get("/", ctx -> ctx.render("index.html"));
+        //CarportController.addRoutes(app, connectionPool);
+        UserController.addRoutes(app, connectionPool);
     }
 }
