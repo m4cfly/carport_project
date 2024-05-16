2307042794f1793473cc61024a2a34ad714c9712
Jenjolen <cph-jn466@cphbusiness.dk>
2024-05-16 20:45:09 +0200

    small changes in Calculator

--- src/main/java/app/services/Calculator.java
+++ src/main/java/app/services/Calculator.java
@@ -60,32 +60,47 @@ public class Calculator {
 
         // Find variant
         List<MaterialVariant> materialVariants = MaterialMapper.getVariantsByProductIdAndMinLength(length, BEAMS, connectionPool);
-        MaterialVariant materialVariant = materialVariants.get(0);
 
         boolean found = false;
+        int lengthDifference = length;
+        MaterialVariant chosenVariant = null;
+
 
         while (!materialVariants.isEmpty()) {
-            for (MaterialVariant variant : materialVariants) {
-                if (length % variant.getLength() == 0) {
-                    int quantity = (length % variant.getLength()) * 2;
-                    Material_Item materialItem = new Material_Item(0, quantity, "Rem placeres ovenpå stolper", order, materialVariant);
-                    materialItems.add(materialItem);
-                    found = true;
-                    break;
-                } else if (variant.getLength() > length) {
-                    Material_Item materialItem = new Material_Item(0, 2, "Rem placeres ovenpå stolper, og resten saves af", order, materialVariant);
+            for (int i = 0; i < materialVariants.size(); i++) {
+                MaterialVariant materialVariant = materialVariants.get(i);
+                boolean foundInCurrentLoop = false;
+                for (MaterialVariant variant : materialVariants) {
+                    if (length % variant.getLength() == 0) {
+                        int quantity = (length % variant.getLength()) * 2;
+                        Material_Item materialItem = new Material_Item(0, quantity, "Rem placeres ovenpå stolper", order, materialVariant);
+                        materialItems.add(materialItem);
+                        found = true;
+                        break;
+                    } else if (variant.getLength() - length < lengthDifference) {
+                        lengthDifference = variant.getLength() - length;
+                        chosenVariant = variant;
+                        foundInCurrentLoop = true;
+                        break;
+                    }
+
+                }
+                if (chosenVariant.getLength() > length && foundInCurrentLoop == true) {
+                    int quantity = (length % chosenVariant.getLength()) * 2;
+                    Material_Item materialItem = new Material_Item(0, quantity, "Rem placeres ovenpå stolper, og resten saves af", order, materialVariant);
                     materialItems.add(materialItem);
                     found = true;
                     break;
                 }
-
             }
+
         }
+
         if (!found) {
 
             List<MaterialVariant> allMaterialVariants = MaterialMapper.getVariantsByProductIdAndMinLength(0, BEAMS, connectionPool);
             int variantSpareWood = length;
-            MaterialVariant chosenVariant = null;
+            chosenVariant = null;
             for (MaterialVariant variant : allMaterialVariants) {
                 if (length == variant.getLength()) {
                     int beamQuantity = (length % variant.getLength()) * 2;
@@ -116,6 +131,7 @@ public class Calculator {
                 materialItems.add(materialItem);
                 found = true;
 
+
             }
 
 
@@ -137,14 +153,13 @@ public class Calculator {
 
         // Find variant
         List<MaterialVariant> materialVariants = MaterialMapper.getVariantsByProductIdAndMinLength(width, RAFTERS, connectionPool);
-        MaterialVariant materialVariant = materialVariants.get(0);
-
 
 
         boolean found = false;
 
         while (!found) {
             for (MaterialVariant variant : materialVariants) {
+                MaterialVariant materialVariant = materialVariants.get(materialVariants.indexOf(variant));
                 if (width == variant.getLength()) {
                     Material_Item materialItem = new Material_Item(0, quantity, "Spær placeres ovenpå rem", order, materialVariant);
                     materialItems.add(materialItem);
@@ -163,15 +178,11 @@ public class Calculator {
         }
 
 
-
-
-
     }
 
     public double calcRaftQuantity() {
         return 2 + (length / (4.5 + 55));
     }
-
 
 
     public List<Material_Item> getMaterialItems() {
