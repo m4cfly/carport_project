144fcd6687e6213f49697d853a0dee11fbf4f1de
Davud05 <Drassuil@gmail.com>
2024-05-24 04:59:59 +0200

    Made Email in Main and updated pom to work with it.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,14 +1,20 @@
 package app;
+
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
 import app.controllers.OrderController;
 import app.controllers.UserController;
-
-
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
-
+import java.io.IOException;
 
 public class Main {
     private static final String USER = "postgres";
@@ -18,22 +24,63 @@ public class Main {
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
-
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
-
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
-            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
         // Routing
-        //CarportController.addRoutes(app, connectionPool);
         OrderController.addRoutes(app, connectionPool);
         UserController.addRoutes(app, connectionPool);
         app.get("/showOrder", ctx -> OrderController.showOrder(ctx));
+
+        // SendGrid email setup
+        sendEmail();
+    }
+
+    private static void sendEmail() {
+        Email from = new Email("Drassuil@gmail.com");
+        from.setName("Johannes Fog Byggemarked");
+
+        Mail mail = new Mail();
+        mail.setFrom(from);
+
+        String API_KEY = System.getenv("SENDGRID_API_KEY");
+
+        if (API_KEY == null) {
+            System.out.println("Error: SENDGRID_API_KEY environment variable not set.");
+            return;
+        }
+
+        Personalization personalization = new Personalization();
+        personalization.addTo(new Email("Drassuil@gmail.com"));
+        personalization.addDynamicTemplateData("name", "David Vig");
+        personalization.addDynamicTemplateData("email", "Drassuil@gmail.com");
+        personalization.addDynamicTemplateData("zip", "2100");
+        mail.addPersonalization(personalization);
+
+        mail.addCategory("carportapp");
+
+        SendGrid sg = new SendGrid(API_KEY);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+
+
+            mail.setTemplateId("d-d75882e1ca844bf18add2bc944aaf40e");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println("Status Code: " + response.getStatusCode());
+            System.out.println("Response Body: " + response.getBody());
+            System.out.println("Response Headers: " + response.getHeaders());
+        } catch (IOException ex) {
+            System.out.println("Error sending mail");
+            ex.printStackTrace();
+
+        }
     }
 }
